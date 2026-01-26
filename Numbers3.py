4c1334d04c6146dbf2b9b8a2207d91737d6dc744
tanmay7506 <tanmay.atts@gmail.com>
2026-01-26 16:36:41 +0530

    Numbers 3
    Arithmetic Operations tuple from two integers
    Given a tuple of two integers (a, b), return a tuple containing the sum, difference, product, and quotient (integer division) of the two numbers.

---
+++ Numbers3.py
@@ -0,0 +1,24 @@
+# Arithmetic Operations tuple from two integers
+# Given a tuple of two integers (a, b), return a tuple containing the sum, difference, product, and quotient (integer division) of the two numbers.
+
+def arithmetic_operations(t: tuple) -> tuple:
+    '''
+    Given a tuple of two integers (a, b), return a tuple containing the 
+    sum, difference, product, and quotient (integer division) of the two numbers.
+
+    Arguments:
+    t: tuple - a tuple of two integers (a, b)
+
+    Return:
+    tuple - a tuple containing the sum, difference, product, and quotient
+
+    Example:
+    >>> arithmetic_operations((1, 2))
+    (3, -1, 2, 0)
+    '''
+    
+    a = t[0]
+    b = t[1]
+    
+    sum, difference, product, quotient = a+b, a-b, a*b, a//b
+    return (sum, difference, product, quotient)
