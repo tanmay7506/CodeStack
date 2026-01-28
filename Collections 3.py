3a1a0e512f8ebb4591779fdcb7e8506472ab37e7
tanmay7506 <tanmay.atts@gmail.com>
2026-01-28 23:55:07 +0530

    Collections 3
    Swap Alternate Elements in a Tuple
    Write a function to swap every alternate of adjacent elements in the tuple.
    
    Assume the length of the tuple is even.

---
+++ Collections 3.py
@@ -0,0 +1,21 @@
+#Write a function to swap every alternate of adjacent elements in the tuple.
+
+def swap_alternate_elements(t):
+    '''Swap every alternate of adjacent elements in the tuple.
+
+    Args:
+        t (tuple): A tuple of even length.
+
+    Returns:
+        tuple: A new tuple with alternate elements swapped.
+
+    Examples:
+    >>> swap_alternate_elements((1, 2, 3, 4, 5, 6))
+    (2, 1, 4, 3, 6, 5)
+    >>> swap_alternate_elements(('a', 'b', 'c', 'd'))
+    ('b', 'a', 'd', 'c')
+    '''
+    lst = list(t)
+    for i in range(0, len(t), 2):
+        lst[i], lst[i+1] = lst[i+1], lst[i]
+    return tuple(lst)
