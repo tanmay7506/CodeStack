d1575b963b763089789871b15161d1b0a3d7a788
tanmay7506 <tanmay.atts@gmail.com>
2026-02-07 23:21:41 +0530

    Collatz Function

---
+++ Practice 3.py
@@ -0,0 +1,21 @@
+# Collatz Function
+
+
+def collatz(n):
+    """
+    A recursive function to compute the number of calls to the Collatz function of n
+
+    Argument:
+        n: integer
+        Assume that 1 < n <= 32,000
+    Returns: 
+        result: integer
+    """
+    if n == 1:
+        return 0
+
+    if n % 2 == 0:
+        return 1 + collatz(n // 2)
+
+    else:
+        return 1 + collatz(3 * n + 1)
